73378335268494f29db325f0c4b0d56429afc3fe
Avram <ad@dermer.co.il>
2025-06-19 15:14:56 +0300

    not time

---
+++ Quadrilaterals/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSPתרגול.Quadrilaterals
+{
+    internal class Rectangle:RectangleABS
+    {
+        public override double Width { get; protected set; }
+        public override double Height { get; protected set; }
+        public Rectangle()
+        {
+            Width = 0;
+            Height = 0;
+        }
+
+        public override void SetWidth(double w)
+        {
+            if(w<0)
+                Width = w;
+        }
+        public override void  SetHeight (double num)
+        {
+            if(num<0)
+                Width = num;
+        }
+    }
+}
---
+++ Quadrilaterals/RectangleABS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSPתרגול.Quadrilaterals
+{
+    abstract class RectangleABS
+    {
+        public abstract double Width { get; protected  set; }
+        public abstract double Height { get; protected set; }
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public abstract void SetWidth(double w);
+        public abstract void SetHeight(double h);
+    }
+}
+
